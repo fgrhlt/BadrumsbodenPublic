479540d55f4516ab0bdf4bd9981262fae07b8be9
AlfredOdling <alfred.odling@hotmail.se>
2016-07-30 11:33:28 +0200

    Added stuff

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import DropdownMenu from './DropdownMenu'
+
 require('normalize.css/normalize.css')
 require('styles/App.css')
 
 export default class AppComponent extends Component {
+  //propTypes={}
+
   render() {
     return (
-      <div className="body">
+      <div>
         <div className="header">
-          <h1>Hej</h1>
+          <p>Tillbaka till portalen</p>
+          <img id="logo" src="images/logo.svg"></img>
+
+          <div className="searchBar">
+            <input placeholder="Vad söker du efter?"></input>
+            <img id="searchBarImg" src="images/search_button.svg"></img>
+          </div>
         </div>
+
+        <DropdownMenu/>
+
+        <div className="campaign">
+          <h1>Hej detta är kampanj jao</h1>
+          <img id="campaign" src="images/campaign.jpg"></img>
+        </div>
+
+        <div className="topSales"></div>
+
       </div>
     )
   }
-}
-
-AppComponent.defaultProps = {
 }
