3d08ef944a8e14c0471bb9909ee8cebc2eab15f5
AlfredOdling <alfred.odling@hotmail.se>
2016-08-01 15:57:59 +0200

    added stuff

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ import Webshop from './components/WebshopPage/Webshop';
 import Gallery from './components/galleryPage/Gallery';
 
 const app = document.getElementById('app')
+const NotFound = () => (
+  <h1>404.. This page is not found!</h1>)
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={LandingPage}>
       <Route path="webshop" component={Webshop}></Route>
       <Route path="gallery" component={Gallery}></Route>
+      <Route path='*' component={NotFound}></Route>
     </Route>
   </Router>,
   app);
