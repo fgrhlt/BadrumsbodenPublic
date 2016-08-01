66d466209044a8028e407a260395db674b99d257
AlfredOdling <alfred.odling@hotmail.se>
2016-08-01 15:57:59 +0200

    added stuff

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -4,13 +4,21 @@ import { Link } from 'react-router'
 require('styles/_landingPage/main.css')
 
 export default class LandingPage extends Component {
+
+  handleClick(e) {
+      e.preventDefault();
+      console.log('er', this.context);
+    }
+
+
   render() {
     return (
-        <div>
-          <h1>Hej!</h1>
-          {this.props.children}
-          <Link to="gallery">To gallery</Link>
-        </div>
+      <div>
+        <h3>Home</h3>
+        <button onClick={this.handleClick}>
+          Navigate outside of component to About page
+        </button>
+      </div>
     )
   }
 }
