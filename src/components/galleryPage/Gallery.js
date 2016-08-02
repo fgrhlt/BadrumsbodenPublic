ded6d012f252c5c10c8741a587a79e5d07cc930a
Simon Asp <simpasp@gmail.com>
2016-08-02 22:20:46 +0200

    lightbox gallery half done not working yet

--- src/components/galleryPage/Gallery.js
+++ src/components/galleryPage/Gallery.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import LightboxGallery from './LightboxGallery'
+import Lightbox from 'react-images';
 import Header from './Header.js'
 
 require('styles/_galleryPage/gallery.css')
 
 export default class Gallery extends Component {
+
+  componentWillMount() {
+    this.setState ({
+      lightboxIsOpen:true
+    })
+  }
+
   render() {
     return (
       <div>
-        <Header></Header>
+        <Header />
 
         <div id="info">
           <div>
@@ -21,38 +30,24 @@ export default class Gallery extends Component {
           </div>
         </div>
 
+        <LightboxGallery
+          images={[{ src: 'http://placekitten.com/404/303' },
+                   { src: 'http://placekitten.com/444/343' }]}
+          isOpen={this.state.lightboxIsOpen}
+          onClickPrev={this.gotoPrevious}
+          onClickNext={this.gotoNext}
+          onClose={this.closeLightbox}
+        />
 
         <div className="container">
           <h3>Badrum</h3>
 
           <section>
             <div>
-              <figure><img src="http://placekitten.com/404/303" /></figure>
-              <figure><img src="http://placekitten.com/405/213" /></figure>
-              <figure><img src="http://placekitten.com/404/210" /></figure>
-            </div>
+              <figure><img src="http://placekitten.com/404/303" />
+                <div>Det här här information om katten som ni ser här hallåå lalalal</div>
+              </figure>
 
-            <div>
-              <figure><img src="http://placekitten.com/404/353" /></figure>
-              <figure><img src="http://placekitten.com/404/210" /></figure>
-              <figure><img src="http://placekitten.com/201/303" /></figure>
-            </div>
-
-            <div>
-              <figure><img src="http://placekitten.com/100/303" /></figure>
-              <figure><img src="http://placekitten.com/200/303" /></figure>
-              <figure><img src="http://placekitten.com/540/303" /></figure>
-            </div>
-
-          </section>
-        </div>
-
-        <div className="container">
-          <h3>Kök</h3>
-
-          <section>
-            <div>
-              <figure><img src="http://placekitten.com/404/303" /></figure>
               <figure><img src="http://placekitten.com/405/213" /></figure>
               <figure><img src="http://placekitten.com/404/210" /></figure>
             </div>
@@ -94,6 +89,7 @@ export default class Gallery extends Component {
             </div>
           </section>
         </div>
+
       </div>
     )
   }
