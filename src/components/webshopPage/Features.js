8799a19d4d41dae1ee15634d644a1006185afad0
Simon Asp <simpasp@gmail.com>
2016-08-03 14:58:05 +0200

    changed some icons, finished feature-box

--- src/components/webshopPage/Features.js
+++ src/components/webshopPage/Features.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react'
 
+require('styles/_webshopPage/features.css')
+
 export default class Features extends Component {
 
   render() {
     return (
       <div className="features">
-        <div className="iconContainer">
-          <div className="circle">
-            <img src="assets/icons/shipping.svg"></img>
-          </div>
 
-          <p>Garanti på alla varor</p>
-        </div>
+        <h2>Trygg handel hos oss</h2>
+
+          <section>
+            <div>
+              <div className="circle">
+                <img src="assets/icons/webshop/shipping.svg" />
+              </div>
+
+              <p>Fraktas inom 5 arbetsdagar</p>
+            </div>
+
+            <div>
+              <div className="circle">
+                <img src="assets/icons/webshop/warranty.svg" />
+              </div>
+
+              <p>Garanti på alla varor</p>
+            </div>
+
+            <div>
+              <div className="circle">
+                <img src="assets/icons/webshop/payment.svg" />
+              </div>
+
+              <p>Säkra betalningsmetoder</p>
+            </div>
+
+            <div>
+              <div className="circle">
+                <img src="assets/icons/webshop/service.svg"></img>
+              </div>
+
+              <h3>hej</h3>
+              <p>Pålitlig service</p>
+            </div>
+          </section>
       </div>
     )
   }
