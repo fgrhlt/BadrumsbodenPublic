ba44bcf997eab2deadb9ea864572a4bfd77322ae
Simon Asp <simpasp@gmail.com>
2016-08-03 17:57:39 +0200

    campaign finished 1.0

---
+++ src/components/webshopPage/Campaign.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+require('styles/_webshopPage/campaign.css')
+
+export default class Campaign extends Component {
+
+  render() {
+    /* Data from database */
+    var styleVar = {
+      backgroundImage: 'url(assets/images/campaign.jpg)',
+      color: '#fff',
+    }
+
+    return (
+      <div id="campaign">
+        <div style={styleVar}>
+
+          <section>
+            <h1>Kampanj</h1>
+            <p>
+              Köp en toalettstol idag och få 200:- rabatt.
+              Du kommer tjäna massor med pengar på detta
+              erbjudande! <br /><br />
+
+              Först till kvarn gäller och allt är bra.
+              Vi finns på Kabelvägen 8. Öppet 8-16
+            </p>
+          </section>
+
+          <div><button className="greenButton">Till erbjudande</button></div>
+        </div>
+
+      </div>
+    )
+  }
+}
