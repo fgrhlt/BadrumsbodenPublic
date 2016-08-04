13bd2346e05c13969394db7efd1d55bf65bac146
Simon Asp <simpasp@gmail.com>
2016-08-04 19:26:04 +0200

    finished features on services page 1.0

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -13,12 +13,17 @@ onClickGallery() {
   browserHistory.push('/gallery')
 }
 
+onClickServices() {
+  browserHistory.push('/services')
+}
+
   render() {
     return (
       <div>
         <h3>LandingPage</h3>
         <button onClick={this.onClickWebshop.bind(this)}>Klicka här för webshop</button>
         <button onClick={this.onClickGallery.bind(this)}>Klicka här för galleri</button>
+        <button onClick={this.onClickServices.bind(this)}>Klicka här för tjänster</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Router, Route, browserHistory } from 'react-router';
 import LandingPage from './components/LandingPage/LandingPage';
 import Webshop from './components/WebshopPage/Webshop';
 import Gallery from './components/galleryPage/Gallery';
+import Services from './components/servicesPage/Services';
+
 
 const app = document.getElementById('app')
 const NotFound = () => (
@@ -15,6 +17,7 @@ ReactDOM.render(
     <Route path="/" component={LandingPage}></Route>
     <Route path="/webshop" component={Webshop}></Route>
     <Route path="/gallery" component={Gallery}></Route>
+    <Route path="/Services" component={Services}></Route>
     <Route path="/*" component={NotFound}></Route>
   </Router>,
   app);
