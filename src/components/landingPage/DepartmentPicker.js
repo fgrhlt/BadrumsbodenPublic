dc4eb0a34825c9f40bdd1352b202d76e0ff5c078
Simon Asp <simpasp@gmail.com>
2016-08-10 21:07:01 +0200

    added landing page and stuff

---
+++ src/components/landingPage/DepartmentPicker.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+require('styles/_landingPage/departmentPicker.css')
+
+export default class departmentPicker extends Component {
+
+  render() {
+
+    return (
+      <div id="departmentPicker">
+        <div>
+          <h1>Webbshop</h1>
+          <div><figure /></div>
+        </div>
+
+        <div>
+          <h1>Tjänster</h1>
+          <div><figure /></div>
+        </div>
+      </div>
+    )
+  }
+}
--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 
 import { browserHistory } from 'react-router'
+import Header from './Header'
+import WelcomeInfo from './WelcomeInfo'
+import DepartmentPicker from './DepartmentPicker'
+
+
 
 export default class LandingPage extends Component {
 
@@ -22,7 +27,11 @@ onClickAdmin() {
   render() {
     return (
       <div>
-        <h3>LandingPage</h3>
+        <Header></Header>
+        <WelcomeInfo></WelcomeInfo>
+        <DepartmentPicker></DepartmentPicker>
+
+
         <button onClick={this.onClickWebshop.bind(this)}>Klicka här för webshop</button>
         <button onClick={this.onClickGallery.bind(this)}>Klicka här för galleri</button>
         <button onClick={this.onClickServices.bind(this)}>Klicka här för tjänster</button>
