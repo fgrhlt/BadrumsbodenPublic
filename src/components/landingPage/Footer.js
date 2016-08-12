2e6c270d24fd041478828af7b4c737a0004f7658
Simon Asp <simpasp@gmail.com>
2016-08-12 21:10:10 +0200

    finished landing page almost

---
+++ src/components/landingPage/Footer.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+
+require('styles/_footerPage/footer.css')
+
+export default class Footer extends Component {
+
+  render() {
+    return (
+      <div id="footer">
+        <section id="landingPage">
+            <div>
+              <figure />
+              <span>
+                Vi använder cookies, för att öka användarvänligheten i enlighet
+                med lagen om elektronisk kommunikation.<br /><br />
+
+                Genom att fortsätta använda vår webplats förutsätter vi att du
+                godkänner detta. <a href="">Läs mer här</a>.
+              </span>
+            </div>
+
+            <div>
+              <h3>Kontakt</h3>
+              <p>
+                Kabelvägen 8 <br />
+                901 33 Umeå <br />
+                090 - 13 13 04 <br />
+                <a href="">info@badrumsboden.se</a><br />
+              </p>
+
+              <h3>Öppettider</h3>
+              <p>
+                Vardagar: 11.00 - 17.00 <br />
+                Lör-Sön: Stängt <br />
+              </p>
+            </div>
+
+            <div>
+              <div>
+                <h3>Om Badrumsboden</h3>
+                <p className="info">
+                  Badrumsboden startades år 2013 av Mats Hammarberg och Jonny Johansson.<br /><br />
+                  Målet med badrumsboden är att erbjuda snabb och enkel service inom
+                  VVS med ledordet snabbhet och proffesinalism.
+                </p>
+              </div>
+
+              <div>
+                <div>
+                  <div className="about" style={{backgroundImage: 'url(assets/images/landingPage/mats.jpg)'}}></div>
+                  <p>Mats Hammarberg</p>
+                </div>
+
+                <div>
+                  <div className="about" style={{backgroundImage: 'url(assets/images/landingPage/jonny.jpg)'}}></div>
+                  <p>Jonny Johansson</p>
+                </div>
+              </div>
+            </div>
+        </section>
+      </div>
+    )
+  }
+}
--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,36 +1,21 @@
 import React, { Component } from 'react'
 
-import { browserHistory } from 'react-router'
 import Header from './Header'
 import WelcomeInfo from './WelcomeInfo'
 import DepartmentPicker from './DepartmentPicker'
-
-
+import Features from './Features'
+import Footer from './Footer'
 
 export default class LandingPage extends Component {
-
-onClickWebshop() {
-  browserHistory.push('/webshop')
-}
-
-onClickGallery() {
-  browserHistory.push('/gallery')
-}
-
-onClickServices() {
-  browserHistory.push('/services')
-}
 
   render() {
     return (
       <div>
         <Header></Header>
         <WelcomeInfo></WelcomeInfo>
-        <DepartmentPicker></DepartmentPicker>
-
-        <button onClick={this.onClickWebshop.bind(this)}>Klicka här för webshop</button>
-        <button onClick={this.onClickGallery.bind(this)}>Klicka här för galleri</button>
-        <button onClick={this.onClickServices.bind(this)}>Klicka här för tjänster</button>
+        <DepartmentPicker ></DepartmentPicker>
+        <Features ></Features>
+        <Footer></Footer>
       </div>
     )
   }
