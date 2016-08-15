baf7a9b84b462a230907303033247d1ac1ec80f0
Simon Asp <simpasp@gmail.com>
2016-08-15 02:37:37 +0200

    added first draft on the checkout, webshop

--- src/components/webshopPage/Webshop.js
+++ src/components/webshopPage/Webshop.js
@@ -9,6 +9,7 @@ import ProductView from './ProductView'
 import Products from './Products'
 import Banner from './Banner'
 import Features from './Features'
+import Checkout from './Checkout'
 import Footer from './Footer'
 
 require('styles/_webshopPage/webshop.css')
@@ -25,7 +26,9 @@ export default class Webshop extends Component {
         { /* <TopSellers></TopSellers> */ }
         { /* <InfoBank></InfoBank>  */ }
         { /* <ProductView></ProductView> */}
-        <Products></Products>
+        { /* <Products></Products> */}
+        <Checkout></Checkout>
+
         <Banner></Banner>
 
         <Features></Features>
