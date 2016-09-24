4b4ae6d1c3c8624e1c565dc0cc5ab31677fa5164
Simon Asp <simpasp@gmail.com>
2016-09-24 13:42:54 +0200

    service slideshow now works

--- src/components/services/ServiceSelector.js
+++ src/components/services/ServiceSelector.js
@@ -3,149 +3,110 @@ require('styles/_servicesPage/serviceSelector2.css')
 
 export default class ServiceSelector extends Component {
 
-  handleClick(e) {
-    let leftService = document.getElementById('leftService')
-    let leftServiceExpanded = document.getElementById('leftServiceExpanded')
-    let leftServiceMinimized = document.getElementById('leftServiceMinimized')
-
-    let rightService = document.getElementById('rightService')
-    let rightServiceExpanded = document.getElementById('rightServiceExpanded')
-    let rightServiceMinimized = document.getElementById('rightServiceMinimized')
-
-    if (leftService.contains(e.target)) {
-      console.log('leftService');
-      leftService.style.display = 'none'
-      leftServiceExpanded.style.display = 'inline'
-      leftServiceMinimized.style.display = 'none'
-
-      rightService.style.display = 'none'
-      rightServiceExpanded.style.display = 'none'
-      rightServiceMinimized.style.display = 'inline'
-    }
-    else if (leftServiceMinimized.contains(e.target)) {
-      console.log('leftServiceMinimized');
-      leftService.style.display = 'inline'
-      leftServiceExpanded.style.display = 'none'
-      leftServiceMinimized.style.display = 'none'
-
-      rightService.style.display = 'inline'
-      rightServiceExpanded.style.display = 'none'
-      rightServiceMinimized.style.display = 'none'
-    }
-    else if (rightService.contains(e.target)) {
-      console.log('rightService');
-      leftService.style.display = 'none'
-      leftServiceExpanded.style.display = 'none'
-      leftServiceMinimized.style.display = 'inline'
-
-      rightService.style.display = 'none'
-      rightServiceExpanded.style.display = 'inline'
-      rightServiceMinimized.style.display = 'none'
-    }
-    else if (rightServiceMinimized.contains(e.target)) {
-      console.log('rightServiceMinimized');
-      leftService.style.display = 'inline'
-      leftServiceExpanded.style.display = 'none'
-      leftServiceMinimized.style.display = 'none'
-
-      rightService.style.display = 'inline'
-      rightServiceExpanded.style.display = 'none'
-      rightServiceMinimized.style.display = 'none'
+  componentWillMount() {
+    this.state = {
+      left: {
+        name: '',
+        innerContent: '',
+        arrow: 'hidden'
+      },
+      right: {
+        name: '',
+        innerContent: '',
+        arrow: 'hidden'
+      }
     }
   }
 
-  expandDiv() {
-    this.props.toggleDiv(true)
+  handleClick(e) {
+    let left = this.refs.left
+    let right = this.refs.right
+
+    /* If you clicked the left or right box and it's minimized, put it to normal state */
+    if((e.currentTarget == left && e.currentTarget.className == 'left minimized')
+    || (e.currentTarget == right && e.currentTarget.className == 'right minimized')) {
+      console.log('TJEEENA')
+      this.setState({
+        left: {
+          name: '',
+          innerContent: '',
+          arrow: 'hidden'
+        },
+        right: {
+          name: '',
+          innerContent: '',
+          arrow: 'hidden'
+        }
+      })
+    }
+    /* If you clicked the left box, expand it and minimize the right */
+    else if(e.currentTarget == left) {
+      this.setState({
+        left: {
+          name: 'left expanded',
+          innerContent: '',
+          arrow: 'hidden'
+        },
+        right: {
+          name: 'right minimized',
+          innerContent: 'hidden',
+          arrow: ''
+        }
+      })
+    }
+    /* If you clicked the right box, expand it and minimize the left */
+    else if(e.currentTarget == right) {
+      this.setState({
+        right: {
+          name: 'right expanded',
+          innerContent: '',
+          arrow: 'hidden'
+        },
+        left: {
+          name: 'left minimized',
+          innerContent: 'hidden',
+          arrow: ''
+        }
+      })
+    }
   }
 
   render() {
-    var containerStyle = {
+    let containerStyle = {
       backgroundImage: 'url(assets/images/services/service_bg.jpg)',
     }
 
     return (
       <div className="serviceSelector" style={containerStyle}>
 
-        <div className="service" id="leftService" onClick={this.handleClick.bind(this)}>
-          <div className="expandArrow">
-            <img src="assets/arrows/expandArrowRight.svg"></img>
-          </div>
+        <div ref="left" className={this.state.left.name} onClick={this.handleClick.bind(this)}>
+          <figure className={this.state.left.arrow} />
 
-          <div className="innerContainer">
-            <figure id="figureLeft" />
+          <div className={this.state.left.innerContent}>
+            <figure name="wrench" />
             <h2>Badrumsrenovering</h2>
             <h3>
               Låt våra badrumsproffs med lång erfarenhet
               utföra din badrumsrenovering. Klicka och läs mer!
             </h3>
           </div>
-
-          <div className="border" id="borderLeft"/>
+          <div className="border" />
         </div>
 
-        <div className="serviceExpanded" id="leftServiceExpanded" onClick={this.handleClick.bind(this)}>
-          <div className="expandArrow">
-            <img src="assets/arrows/expandArrowRight.svg"></img>
-          </div>
+        <div ref="right" className={this.state.right.name} onClick={this.handleClick.bind(this)}>
+          <figure className={this.state.right.arrow} />
 
-          <div className="innerContainer" onClick={this.expandDiv.bind(this)}>
-            <figure id="figureLeft" />
-            <h2>Badrumsrenovering</h2>
-            <h3>
-              Låt våra badrumsproffs med lång erfarenhet
-              utföra din badrumsrenovering. Klicka och läs mer!
-            </h3>
-          </div>
-
-          <div className="border" id="borderLeft"/>
-        </div>
-
-        <div className="serviceMinimized" id="leftServiceMinimized" onClick={this.handleClick.bind(this)}>
-          <div id="expandArrow">
-            <img src="assets/arrows/expandArrowRight.svg"></img>
-          </div>
-        </div>
-
-
-        <div className="service" id="rightService" onClick={this.handleClick.bind(this)}>
-          <div className="expandArrow">
-            <img src="assets/arrows/expandArrowLeft.svg"></img>
-          </div>
-
-          <div className="innerContainer">
-            <figure id="figureRight"/>
+          <div className={this.state.right.innerContent}>
+            <figure name="tap"/>
             <h2>VVS</h2>
             <h3>
               Låt våra badrumsproffs med lång erfarenhet
               utföra din badrumsrenovering. Klicka och läs mer!
             </h3>
           </div>
-
-          <div className="border" id="borderRight"/>
+          <div className="border" />
         </div>
 
-        <div className="serviceExpanded" id="rightServiceExpanded" onClick={this.handleClick.bind(this)}>
-          <div className="expandArrow">
-            <img src="assets/arrows/expandArrowLeft.svg"></img>
-          </div>
-
-          <div className="innerContainer">
-            <figure id="figureRight"/>
-            <h2>VVS</h2>
-            <h3>
-              Låt våra badrumsproffs med lång erfarenhet
-              utföra din badrumsrenovering. Klicka och läs mer!
-            </h3>
-          </div>
-
-          <div className="border" id="borderRight"/>
-        </div>
-
-        <div className="serviceMinimized" id="rightServiceMinimized" onClick={this.handleClick.bind(this)}>
-          <div id="expandArrow">
-            <img src="assets/arrows/expandArrowLeft.svg"></img>
-          </div>
-        </div>
       </div>
     )
   }
