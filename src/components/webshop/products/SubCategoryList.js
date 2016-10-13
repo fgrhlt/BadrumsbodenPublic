fa309918bef1efe43733f9184cb981339d4dfd68
Simon Asp <simpasp@gmail.com>
2016-10-13 12:00:42 +0200

    La till produktmapp i webshop

---
+++ src/components/webshop/products/SubCategoryList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { browserHistory } from 'react-router'
+require('styles/_webshopPage/products.css')
+
+export default class SubCategoryList extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <ul>
+          <li>Hej!</li>
+          <li>din lilla</li>
+          <li>apa</li>
+          <li>och så vidare</li>
+        </ul>
+      </div>
+    )
+  }
+}
