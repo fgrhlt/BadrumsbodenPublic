e6c0dc4a88b5c931cac858768c5b0b58a9b6cef9
AlfredOdling <alfred.odling@hotmail.se>
2016-10-13 11:58:42 +0200

    hej

---
+++ src/components/webshop/topSellers/ProductElements.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import TopSellerProduct from './TopSellerProduct'
+require('styles/_webshopPage/products.css')
+
+export default class ProductElements extends Component {
+
+  render() {
+    return (
+      <div id="productlist1">
+
+        {this.props.items.map((item) => {
+          return <TopSellerProduct key={item.key} item={item}/>
+        })}
+
+      </div>
+    )
+  }
+}
--- src/compositions/webshopPage/WebshopHome.js
+++ src/compositions/webshopPage/WebshopHome.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 import * as firebaseActions from '../../actions/firebaseActions'
 
 import Campaign from '../../components/webshop/Campaign'
-import TopSellers from '../../components/webshop/TopSellers'
+import TopSellers from '../../components/webshop/topSellers/TopSellers'
 import Banner from '../../components/webshop/Banner'
 import Features from '../../components/webshop/Features'
 
