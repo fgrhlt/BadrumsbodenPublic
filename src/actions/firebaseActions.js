d30067f3ac9cf2899c764f0fc61330c37a19549b
AlfredOdling <alfred.odling@hotmail.se>
2016-11-01 11:32:08 +0100

    hej

--- src/actions/firebaseActions.js
+++ src/actions/firebaseActions.js
@@ -9,54 +9,62 @@ export function fetchFirebaseData(path, query, searchString) {
 
   if(path=='products'){
     ref = firebase.database()
-      .ref()
-      .child('webshop/produkter')
-      .orderByChild(query)
-      .equalTo(searchString)
-      }else {
-        ref = firebase.database()
-          .ref()
-          .child(path)
-        }
+    .ref()
+    .child('webshop/produkter')
+    .orderByChild(query)
+    .equalTo(searchString)
+  }
+  else if(path=='categories'){
+    ref = firebase.database()
+    .ref()
+    .child('webshop/categories')
+    .orderByChild(query)
+    .equalTo(searchString)
+  }
+  else {
+    ref = firebase.database()
+    .ref()
+    .child(path)
+  }
 
-    return (dispatch) => {
-      ref.once('value', (snapshot) => {
-        var items = []
-        // Loop through {objects} in order
-          snapshot.forEach( (childSnapshot) => {
-            //The object
-            var item = childSnapshot.val()
-            //Get the key of object and push into object
-            item['key'] = childSnapshot.key
-            //Push object to array with items
-            items.push(item)
-          })
-
-        dispatch({
-          type: FETCH_FIREBASE_DATA,
-          folder: query ? path+'/'+query : path,
-          items
-        })
+  return (dispatch) => {
+    ref.once('value', (snapshot) => {
+      var items = []
+      // Loop through {objects} in order
+      snapshot.forEach( (childSnapshot) => {
+        //The object
+        var item = childSnapshot.val()
+        //Get the key of object and push into object
+        item['key'] = childSnapshot.key
+        //Push object to array with items
+        items.push(item)
       })
-    }
+      
+      dispatch({
+        type: FETCH_FIREBASE_DATA,
+        folder: path,
+        items
+      })
+    })
+  }
 }
 
 export function deleteFirebaseElement(product) {
   console.log(product.key);
   var databaseRef = firebase.database()
+  .ref()
+  .child('webshop/produkter/'+product.key)
+  .remove()
+
+  .then(() => {
+    console.log('Database: '+product.folder+'/'+product.key, 'deleted!')
+    var storageRef = firebase.storage()
     .ref()
-    .child('webshop/produkter/'+product.key)
-    .remove()
+    .child('webshop/produkter/'+product.filename)
+    .delete()
 
-    .then(() => {
-      console.log('Database: '+product.folder+'/'+product.key, 'deleted!')
-      var storageRef = firebase.storage()
-      .ref()
-      .child('webshop/produkter/'+product.filename)
-      .delete()
-
-      console.log('Storage: '+product.folder+'/'+product.filename, 'deleted!')
-    })
+    console.log('Storage: '+product.folder+'/'+product.filename, 'deleted!')
+  })
 
   return (dispatch) => {
     dispatch({
