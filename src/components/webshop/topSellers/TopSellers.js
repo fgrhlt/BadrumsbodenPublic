9b6b5e7e49fcc79acc280a38cc0310ed763ba7d6
Simon Asp <simpasp@gmail.com>
2016-11-01 15:45:21 +0100

    hej version 2

--- src/components/webshop/topSellers/TopSellers.js
+++ src/components/webshop/topSellers/TopSellers.js
@@ -5,44 +5,13 @@ require('styles/_webshopPage/topSellers.css')
 
 export default class TopSellers extends Component {
 
-  componentWillMount() {
-    this.state = {
-      items: []
-    }
-  }
-
   render() {
     return (
       <div id="topSellers">
-
         <h4>Toppsäljare</h4>
 
         <div id="products">
           <ProductElements items={this.props.items}/>
-
-          {/*<ProductElements items={items}/>*/}
-
-
-{/*          <div>
-            <figure style={{backgroundImage: 'url(assets/images/webshop/img2.jpg)'}} />
-            <h4>IFÖ Hylla</h4>
-            <p>Metallstomme</p>
-            <span>599:-</span>
-          </div>
-
-          <div>
-            <figure style={{backgroundImage: 'url(assets/images/webshop/img3.jpg)'}} />
-            <h4>IFÖ blandare FG5</h4>
-            <p>Titan</p>
-            <span>49:-</span>
-          </div>
-
-          <div>
-            <figure style={{backgroundImage: 'url(assets/images/webshop/img4.jpg)'}} />
-            <h4>IFÖ tvättställ DE4</h4>
-            <p>Keramik</p>
-            <span>10 000:-</span>
-          </div>*/}
         </div>
       </div>
     )
