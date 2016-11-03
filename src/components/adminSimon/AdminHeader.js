9be6e0732e2eea02bb9a55bd28d1ec6010836931
AlfredOdling <alfred.odling@hotmail.se>
2016-11-03 13:25:26 +0100

    added login

--- src/components/adminSimon/AdminHeader.js
+++ src/components/adminSimon/AdminHeader.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react'
 import { browserHistory } from 'react-router'
 import {replaceSpecialCharactersURLs} from '../../utils/Utils'
 
+import firebase from 'firebase/app'
+require('firebase/auth')
+
 require('styles/_adminSimon/adminHeader.css')
 
 export default class AdminHeader extends Component {
+
+  this.state = {
+    
+  }
 
   /* Get the text that's in the div being clicked. Push it to the router */
   handleClick(e) {
@@ -17,6 +24,11 @@ export default class AdminHeader extends Component {
     browserHistory.push('/newAdmin/')
   }
 
+  userInfo() {
+    firebase.auth().onAuthStateChanged( (user) => {
+
+  }
+
   render() {
     /* The header of the admin page. The two buttons get different background color depending on the URL */
     return (
@@ -24,6 +36,7 @@ export default class AdminHeader extends Component {
         <section id="lostItems">
           <div>
             <figure style={{cursor: 'pointer'}} onClick={this.handleClickLogo.bind(this)} id="logo" />
+
           </div>
 
           <div className={this.props.param=='webshop'?'yellow':'passive'} onClick={this.handleClick.bind(this)}>
---
+++ src/compositions/adminPage/Login.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { browserHistory } from 'react-router'
+
+import firebase from 'firebase/app'
+require('firebase/auth')
+
+export default class Login extends Component {
+
+  componentWillMount() {
+    this.state = {
+      message: ''
+    }
+
+    firebase.auth().onAuthStateChanged( (user) => {
+      if (user) {
+        // User is signed in.
+        this.setState({
+          message: 'User '+user.email+' is signed in'
+        })
+
+        const location = this.props.location
+        if (location.state && location.state.nextPathname) {
+          browserHistory.push(location.state.nextPathname)
+        } else {
+          browserHistory.push('/')
+        }
+      } else {
+        // No user is signed in.
+        this.setState({
+          message: 'No user is signed in'
+        })
+      }
+    })
+  }
+
+
+  handleSubmit(log, event) {
+    event.preventDefault()
+
+    const email = this.refs.email.value
+    const pass = this.refs.pass.value
+
+    if (log=='login') {
+      const promise = firebase.auth().signInWithEmailAndPassword(email, pass)
+
+      promise.catch( (e) => {
+        this.setState({
+          message: e.message
+        })
+      })
+    } else if (log=='logout') {
+      const promise = firebase.auth().signOut()
+      promise.catch( (e) => {console.log(e.message)})
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <input ref="email" placeholder="email" defaultValue="00badrumsboden@gmail.com" /> <br/>
+        <input ref="pass" placeholder="Skriv in lösenord" /><br/>
+
+        <button onClick={this.handleSubmit.bind(this, 'login')}>login</button>
+        <button onClick={this.handleSubmit.bind(this, 'logout')}>logout</button>
+
+        {this.state.message}
+      </div>
+    )
+  }
+}
