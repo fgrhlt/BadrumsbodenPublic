84f1cc035e6bbd39c19e28457042cb7e7f960911
AlfredOdling <alfred.odling@hotmail.se>
2016-11-16 16:51:12 +0100

    fd

--- cfg/dist.js
+++ cfg/dist.js
@@ -32,7 +32,7 @@ let config = Object.assign({}, baseConfig, {
 // Add needed loaders to the defaults here
 config.module.loaders.push({
   test: /\.(js|jsx)$/,
-  loader: 'babel',
+  loader: 'babel-core',
   include: [].concat(
     config.additionalPaths,
     [ path.join(__dirname, '/../src') ]
