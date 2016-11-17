93ae96c4ae52b5610ebdff7428c720ca45a3e1a3
AlfredOdling <alfred.odling@hotmail.se>
2016-11-17 16:26:20 +0100

    f

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 const open = require('open');
 
+console.log('process.env.PORT', process.env.PORT);
+
 new WebpackDevServer(webpack(config), config.devServer)
 .listen(process.env.PORT, 'localhost', (err) => {
   if (err) {
