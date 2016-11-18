731e4104cb7aa61c4470abfd86082b5dbdd6e920
AlfredOdling <alfred.odling@hotmail.se>
2016-11-18 16:51:05 +0100

    f

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname+'/dist'))
 
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 //Initialize app
-var server = app.listen(process.env.PORT || 5000, function () {
-  var port = server.address().port
-  console.log("App now running on port", port)
-})
+// var server = app.listen(process.env.PORT || 5000, function () {
+//   var port = server.address().port
+//   console.log("App now running on port", port)
+// })
 
 // //Listen for all APIs
 // app.get('*', function(req, res) {
