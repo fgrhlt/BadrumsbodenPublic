c351c1cdfe5b12f4f49f75a725878bf161a05a55
Alfred Ö <alfred.odling@hotmail.se>
2016-12-01 14:49:05 +0100

    SEO

--- testserver.js
+++ testserver.js
@@ -223,7 +223,10 @@ app.post("/email/priskalkyl", function(req, res) {
       console.log(response.headers);
     });
   });
-
+//google135f1ae3ff3278da.html
+app.get('/google135f1ae3ff3278da.html', function(req, res) {
+ res.sendFile(path.resolve(__dirname, 'google135f1ae3ff3278da.html'))
+})
 app.get("*", function(req, res) {
  res.sendFile(path.resolve(__dirname, 'dist/index.html'))
 })
